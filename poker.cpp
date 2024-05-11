8cab787ad581c953e56d0c334834b038eac1cc30
Muhammad Dzaki Nurhidayat <nurhidayatdzaki695@gmail.com>
2024-05-11 18:58:13 +0700

    :tada:Mulai Proyek

---
+++ poker.cpp
@@ -0,0 +1,8 @@
+/*
+file               : poker.cpp
+author             : Muhammad Dzaki Nurhidayat (231511083)
+                     Muhammad Fachry Khairiansyah (231511084)
+                     Restu Akbar (231511088)
+program description: Program tugas besar mata kuliah Struktur Data dan Algoritma (praktik).
+                     Program Games Poker Tongkrongan.                 
+*/
---
+++ poker.h
@@ -0,0 +1,9 @@
+/*
+/*
+file               : poker.h
+author             : Muhammad Dzaki Nurhidayat (231511083)
+                     Muhammad Fachry Khairiansyah (231511084)
+                     Restu Akbar (231511088)
+program description: Program tugas besar mata kuliah Struktur Data dan Algoritma (praktik).
+                     Program Games Poker Tongkrongan.                 
+*/
